2ac3ad7534615500fb85dfca4887d6ef33e91413
Talbot <talbotlawrence75@gmail.com>
2017-01-19 19:45:20 -0600

    added mistakes to index.html and javascript/

---
+++ javascript/song.js
@@ -0,0 +1,9 @@
+var songs = [];
+
+// songs[0] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[1] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[2] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[3] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[4] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";")"
+
+console.log("songs: ", songs);
